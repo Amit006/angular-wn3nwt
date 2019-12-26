ec34893c4919dd1803f72cb8289f6ed0be444d2a
Amit006 <amitnk19@gmail.com>
2019-12-26 12:53:32 +0530

    Http Client Module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DataTablesModule, DataTableDirective} from "angular-datatables";
+import { HttpClientModule } from '@angular/common/http'; 
+
+
 import { HttpModule } from '@angular/http';
 import { UsersService } from './users.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, DataTablesModule, HttpModule ],
+  imports:      [ BrowserModule, FormsModule, DataTablesModule, HttpModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UsersService]
